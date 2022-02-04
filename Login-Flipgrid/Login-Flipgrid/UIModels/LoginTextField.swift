99ff87ae00c56575e8d48dca1387481d96b8a5d7
Nikilesh Balaji <nikilesh122@gmail.com>
2022-02-03 21:16:15 -0500

    intial screen almost complete Remaining Button

---
+++ Login-Flipgrid/Login-Flipgrid/UIModels/LoginTextField.swift
@@ -0,0 +1,52 @@
+//
+//  LoginTextField.swift
+//  Login-Flipgrid
+//
+//  Created by nikilesh balaji on 2/3/22.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class LoginTextField: UITextField {
+    
+    /// set placeholder Text
+    var placeHolderText: String = "hello"
+    
+    /// set border width
+    var borderWidth: CGFloat = 0.4
+    
+    /// set border color
+    var borderColor: CGColor = UIColor.gray.cgColor
+    
+    /// set corner radius
+    var cornerRadius: CGFloat?
+    
+    override init(frame: CGRect){
+            super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+    }
+    
+    func setup() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = self.frame.size.height / 5
+        self.layer.borderColor = borderColor
+        self.layer.borderWidth = borderWidth
+        self.attributedPlaceholder = NSAttributedString(string: placeHolderText,
+                                attributes: [NSAttributedString.Key.foregroundColor:
+                                UIColor.init(displayP3Red: 105/255, green: 105/255, blue: 105/255, alpha: 1),
+                                NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16) ])
+        self.font = UIFont.boldSystemFont(ofSize: 16)
+    }
+
+    
+}
